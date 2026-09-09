3a15c9993e1c5cd99ddf433e9e3dfe8ed2d1ebc3
Atakancev <149818939+Atakancev@users.noreply.github.com>
2026-09-10 01:49:45 +0300

    legal: render gameplay clarifications on canonical and localized terms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import GameplayEconomyRuleNotice from "./tycoonx-legal/GameplayEconomyRuleNotice";
 import CompanyCommerceRuleNotice from "./tycoonx-legal/CompanyCommerceRuleNotice";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <GameplayEconomyRuleNotice />
         <CompanyCommerceRuleNotice />
       </body>
     </html>
